ce180b9910ce861211424ece9c9a63565113931b
Nazim Uddin <nushourav4@gmail.com>
2021-11-12 16:43:23 +0900

    ok

--- index.js
+++ index.js
@@ -25,10 +25,34 @@ async function run() {
     const propertyCollection = database.collection("properties");
     const usersCollection = database.collection("users");
     const bookingInfo = database.collection("booking_data");
+    const agent = database.collection("agent");
+    const review = database.collection("review");
 
     //get properties
     app.get("/properties", async (req, res) => {
       const cursor = propertyCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+      res.send(result);
+    });
+    //get agent
+    app.get("/agent", async (req, res) => {
+      const cursor = agent.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+      res.send(result);
+    });
+    //get reviews
+    app.get("/reviews", async (req, res) => {
+      const cursor = review.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+      res.send(result);
+    });
+
+    //get all booking
+    app.get("/bookings", async (req, res) => {
+      const cursor = bookingInfo.find({});
       const result = await cursor.toArray();
       res.json(result);
       res.send(result);
@@ -52,7 +76,7 @@ async function run() {
       res.json(result);
     });
 
-    //user booking
+    //user booking email
     app.get("/bookings", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -64,52 +88,56 @@ async function run() {
 
     //id parameter delete
     app.delete("/properties/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await propertyCollection.deleteOne(query);
-        console.log(result);
-        res.send(result);
-        res.json(result);
-      });
-      //update api with id
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await propertyCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+      res.json(result);
+    });
+    //update api with id
     app.put("/properties/:id", async (req, res) => {
-        const id = req.params.id;
-        const updateData = req.body;
-        console.log("hiited update api");
-        const query = { _id: ObjectId(id) };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: {
-            name: updateData.name,
-            address: updateData.address,
-            description: updateData.description,
-            price: updateData.price,
-            img: updateData.img,
-            stay: updateData.stay,
-          },
-        };
-        const result = await propertyCollection.updateOne(filter, updateDoc, options);
-        console.log(result);
-        res.send(result);
-        res.json(result);
-      });
+      const id = req.params.id;
+      const updateData = req.body;
+      console.log("hiited update api");
+      const query = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updateData.name,
+          address: updateData.address,
+          description: updateData.description,
+          price: updateData.price,
+          img: updateData.img,
+          stay: updateData.stay,
+        },
+      };
+      const result = await propertyCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+      res.send(result);
+      res.json(result);
+    });
 
-      //post api
-      app.post("/properties", async (req, res) => {
-        const service = req.body;
-        console.log(service);
-        const result = await propertyCollection.insertOne(service);
-        res.json(result);
-      });
-      //user order api
-      app.post("/order", async (req, res) => {
-        const order = req.body;
-        console.log(order);
-        console.log("order api hited");
-        const result = await order_user.insertOne(order);
-        res.json(result);
-        res.send("order api hitted");
-      });
+    //post api
+    app.post("/properties", async (req, res) => {
+      const service = req.body;
+      console.log(service);
+      const result = await propertyCollection.insertOne(service);
+      res.json(result);
+    });
+    //user order api
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      console.log(order);
+      console.log("order api hited");
+      const result = await order_user.insertOne(order);
+      res.json(result);
+      res.send("order api hitted");
+    });
 
     //check admin user
     app.get("/users/:email", async (req, res) => {
@@ -145,23 +173,14 @@ async function run() {
       res.json(result);
     });
 
-    // app.put('/users/admin', async (req, res) => {
-    //     const user = req.body;
-    //     const requester = req.decodedEmail;
-    //     if (requester) {
-    //         const requesterAccount = await usersCollection.findOne({ email: requester });
-    //         if (requesterAccount.role === 'admin') {
-    //             const filter = { email: user.email };
-    //             const updateDoc = { $set: { role: 'admin' } };
-    //             const result = await usersCollection.updateOne(filter, updateDoc);
-    //             res.json(result);
-    //         }
-    //     }
-    //     else {
-    //         res.status(403).json({ message: 'you do not have access to make admin' })
-    //     }
-
-    // })
+    //make admin role
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
